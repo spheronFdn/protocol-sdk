945070d3bcc1956817622a7327bd5a28f1f2a0fb
rwithik-spheron <rwithik@spheron.network>
2024-07-29 14:33:53 +0530

    Add default page size in config (#3)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-import { DAI, USDC, USDT, WETH } from "@contracts/addresses";
+import { DAI, USDC, USDT, WETH } from '@contracts/addresses';
 
 // Testnet URLs
-export const SPHERON_TESTNET_RPC_URL = "https://sepolia-rollup.arbitrum.io/rpc";
-export const SPHERON_TESTNET_EXPLORER_URL = "https://sepolia.arbiscan.io/";
+export const SPHERON_TESTNET_RPC_URL = 'https://sepolia-rollup.arbitrum.io/rpc';
+export const SPHERON_TESTNET_EXPLORER_URL = 'https://sepolia.arbiscan.io/';
 
 export interface IToken {
   id: number;
@@ -25,31 +25,31 @@ export interface INetwork {
   blockExplorerUrls: string[];
 }
 
-export type NetworkType = "testnet" | "mainnet";
+export type NetworkType = 'testnet' | 'mainnet';
 
 export const networkMap: Record<NetworkType, INetwork> = {
   testnet: {
     chainId: 421614,
-    chainName: "Arbitrum Sepolia",
-    rpcUrls: ["https://sepolia-rollup.arbitrum.io/rpc"],
+    chainName: 'Arbitrum Sepolia',
+    rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
     nativeCurrency: {
-      name: "Ethereum",
-      symbol: "ETH",
+      name: 'Ethereum',
+      symbol: 'ETH',
       decimals: 18,
     },
-    blockExplorerUrls: ["https://sepolia.arbiscan.io/"],
+    blockExplorerUrls: ['https://sepolia.arbiscan.io/'],
   },
   // TODO: NEED TO UPDATE WHEN MAINNET RELEASE
   mainnet: {
     chainId: 421614,
-    chainName: "Arbitrum Sepolia",
-    rpcUrls: ["https://sepolia-rollup.arbitrum.io/rpc"],
+    chainName: 'Arbitrum Sepolia',
+    rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
     nativeCurrency: {
-      name: "Ethereum",
-      symbol: "ETH",
+      name: 'Ethereum',
+      symbol: 'ETH',
       decimals: 18,
     },
-    blockExplorerUrls: ["https://sepolia.arbiscan.io/"],
+    blockExplorerUrls: ['https://sepolia.arbiscan.io/'],
   },
 };
 
@@ -57,32 +57,32 @@ export const tokenMap: Record<NetworkType, IToken[]> = {
   testnet: [
     {
       id: 1,
-      name: "Tether USD Test Token",
-      symbol: "USDT",
+      name: 'Tether USD Test Token',
+      symbol: 'USDT',
       decimal: 6,
       address: USDT,
       // logo: USDTIcon,
     },
     {
       id: 2,
-      name: "DAI Test Token",
-      symbol: "DAI",
+      name: 'DAI Test Token',
+      symbol: 'DAI',
       decimal: 18,
       address: DAI,
       // logo: DaiIcon,
     },
     {
       id: 3,
-      name: "Test Token USD Coin",
-      symbol: "USDC",
+      name: 'Test Token USD Coin',
+      symbol: 'USDC',
       decimal: 6,
       address: USDC,
       // logo: USDCIcon,
     },
     {
       id: 4,
-      name: "Wrapped ETH Test Token",
-      symbol: "WETH",
+      name: 'Wrapped ETH Test Token',
+      symbol: 'WETH',
       decimal: 18,
       address: WETH,
       // logo: WethIcon,
@@ -91,5 +91,6 @@ export const tokenMap: Record<NetworkType, IToken[]> = {
   mainnet: [],
 };
 
-export const networkType =
-  (process.env.NEXT_PUBLIC_NETWORK_TYPE as string) || "testnet";
+export const networkType = (process.env.NEXT_PUBLIC_NETWORK_TYPE as string) || 'testnet';
+
+export const DEFAULT_PAGE_SIZE = 10;
