cdeccc9a8069afd7f500159fa66a2630b7bb3033
AnuragSpheron <anurag@spheron.network>
2024-08-01 18:23:56 +0530

    Added provider module

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { OrderModule } from '@modules/order';
 import { EscrowModule } from '@modules/escrow';
 import { SpheronProviderModule } from '@modules/spheron-provider';
 import { SPHERON_TESTNET_RPC_URL, SPHERON_TESTNET_WSS_URL } from '@config/index';
+import { ProviderModule } from '@modules/provider';
 
 export class SpheronSDK {
   public leases: LeaseModule;
   public orders: OrderModule;
   public escrow: EscrowModule;
+  public provider: ProviderModule;
   public spheronProvider: SpheronProviderModule;
 
   constructor(providerUrl: string = '', proxyUrl: string = '') {
@@ -17,10 +19,12 @@ export class SpheronSDK {
     this.leases = new LeaseModule(provider);
     this.orders = new OrderModule(provider, websocketProvider);
     this.escrow = new EscrowModule(provider);
+    this.provider = new ProviderModule(provider);
     this.spheronProvider = new SpheronProviderModule(providerUrl, proxyUrl);
   }
 }
 
+export * from '@modules/provider/types';
 export * from '@modules/lease/types';
 export * from '@modules/order/types';
 export * from '@modules/escrow/types';
---
+++ src/modules/provider/index.ts
@@ -0,0 +1,62 @@
+import ProviderRegistryAbi from '@contracts/abis/ProviderRegistry.json';
+import { ProviderRegistry } from '@contracts/addresses';
+import { ethers } from 'ethers';
+import { Category, IProvider } from './types';
+
+export class ProviderModule {
+  private provider: ethers.Provider;
+
+  constructor(provider: ethers.Provider) {
+    this.provider = provider;
+  }
+
+  async getProviderDetails(providerAddress: string) {
+    const contractAbi = ProviderRegistryAbi;
+    const contractAddress = ProviderRegistry;
+
+    const contract = new ethers.Contract(contractAddress, contractAbi, this.provider);
+    const response = await contract.getProviderByAddress(providerAddress);
+
+    const providerDetailsData: IProvider = {
+      name: response[0],
+      region: response[1],
+      attributes: response[2],
+      hostUri: response[3],
+      certificate: response[4],
+      paymentsAccepted: response[5],
+      status: response[6].toString(),
+      trust: Number(response[7].toString()) + 1,
+      timestamp: Number(response[8].toString()),
+    };
+
+    return providerDetailsData;
+  }
+
+  async getProviderPendingAttributes(providerAddress: string, category: Category) {
+    if (!category) {
+      console.log('Please pass a category');
+      return;
+    }
+    const contractAbi = ProviderRegistryAbi;
+    const contractAddress = ProviderRegistry;
+
+    const contract = new ethers.Contract(contractAddress, contractAbi, this.provider);
+    const response = await contract.getProviderPendingAttributes(providerAddress, category);
+
+    return response;
+  }
+
+  async getProviderAttributes(providerAddress: string, category: Category) {
+    if (!category) {
+      console.log('Please pass a category');
+      return;
+    }
+    const contractAbi = ProviderRegistryAbi;
+    const contractAddress = ProviderRegistry;
+
+    const contract = new ethers.Contract(contractAddress, contractAbi, this.provider);
+    const response = await contract.getProviderAttributes(providerAddress, category);
+
+    return response;
+  }
+}
