268e9b0d4c46073b0d9eff1709bbd67696a65c31
nitin-spheron <nitin@spheron.network>
2024-09-04 19:19:27 +0530

    Wip: add getAllProviders

--- src/modules/provider/index.ts
+++ src/modules/provider/index.ts
@@ -104,4 +104,77 @@ export class ProviderModule {
       return error;
     }
   }
+
+  async getAllProviders() {
+    try {
+      const contractAbi = ProviderRegistryAbi;
+      const contractAddress = ProviderRegistry;
+
+      const contract = new ethers.Contract(contractAddress, contractAbi, this.provider);
+      const responses: any[] = await contract.getAllProviders();
+
+      const filteredResponses = responses.filter(
+        (response) => response[6] !== 'localhost' && response[1] && response[1].trim() !== ''
+      );
+
+      const allProviderDetails = await Promise.all(
+        filteredResponses.map(async (response) => {
+          let specs: StatusResponse = {
+            totalCPUs: 0,
+            totalMemory: 0,
+            totalStorage: 0,
+            gpuInfos: [],
+          };
+          let version = '-';
+          if (response.status.toString() === '2') {
+            specs = await getProviderStatus(response[6]);
+            version = await getProviderVersion(response[6]);
+          }
+          const isRegistered = Number(response.status) === 1;
+          const perEraRewardData = await getProviderRewardsPerEra(
+            response.walletAddress,
+            isRegistered,
+            TrustTierMultiplier[Number(response[9].toString()) + 1]
+          );
+          const totalRewardsData = await getTotalRewards(
+            response.walletAddress,
+            isRegistered,
+            perEraRewardData
+          );
+          return {
+            id: response[0].toString(),
+            name: response[1],
+            region: response[2],
+            address: response[3],
+            hostUri: response[6],
+            status: response[8].toString(),
+            trust: Number(response[9].toString()) + 1,
+            specs,
+            perEraRewardData,
+            totalRewardsData,
+            timestamp: Number(response.joinTimestamp.toString()),
+            version,
+          };
+        })
+      );
+
+      // Sort providers by total rewards (assuming totalRewardsData is a number)
+      const sortedProviders = allProviderDetails.sort(
+        (a, b) => Number(b.totalRewardsData.toString()) - Number(a.totalRewardsData.toString())
+      );
+
+      const sortedProvidersWithRank = sortedProviders.map((item, index) => ({
+        ...item,
+        rank: index + 1,
+      }));
+
+      const totalFilteredCount = sortedProviders.length;
+      return {
+        providers: sortedProvidersWithRank,
+        totalCount: totalFilteredCount,
+      };
+    } catch (error) {
+      console.log('Error in getting provider details -> ', error);
+    }
+  }
 }
