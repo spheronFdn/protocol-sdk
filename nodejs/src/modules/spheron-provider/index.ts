b1cc231bb9b1c46dc9ccc0f3ae2549d5484ce6af
AnuragSpheron <160430112+AnuragSpheron@users.noreply.github.com>
2024-12-06 18:30:38 +0530

    Revert "Add startup support for logs (#35)"
    This reverts commit e9386e2a48dcb0d13838f86b59503f169de7d4f5.

--- nodejs/src/modules/spheron-provider/index.ts
+++ nodejs/src/modules/spheron-provider/index.ts
@@ -147,18 +147,18 @@ export class SpheronProviderModule {
     authToken: string,
     leaseId: string,
     service = '',
-    tail = 100000,
-    startup = false
+    tail = 100000
   ) {
     if (!leaseId) {
       console.log('Lease ID not found');
       return;
     }
+
     const reqBody = {
       // eslint-disable-next-line prettier/prettier
       url: `${
         this.providerHostUrl
-      }/lease/${leaseId}/${GSEQ}/${OSEQ}/logs?follow=false&tail=${tail}&startup=${startup}${
+      }/lease/${leaseId}/${GSEQ}/${OSEQ}/logs?follow=false&tail=${tail}${
         service ? `&service=${service}` : ''
       }`,
       method: 'GET',
