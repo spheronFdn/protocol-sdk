016f8783cce70b0e6e0a9ec39a310f46a820cee2
AnuragSpheron <160430112+AnuragSpheron@users.noreply.github.com>
2025-07-16 18:12:43 +0530

    Merge pull request #80 from spheronFdn/singingapple/pro-2038
    Replace uri domain with provider host uri

--- nodejs/src/utils/index.ts
+++ nodejs/src/utils/index.ts
@@ -104,3 +104,24 @@ export const initializeSigner = async ({ wallet }: { wallet?: ethers.Wallet }) =
 
   return { signer };
 };
+
+export const replaceDomain = (url: string, newDomain: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const port = parsedUrl.port ? `:${parsedUrl.port}` : '';
+    let pathname = parsedUrl.pathname;
+
+    if (pathname.endsWith('/') && pathname !== '/') {
+      pathname = pathname.slice(0, -1);
+    }
+
+    if (pathname === '/') {
+      pathname = '';
+    }
+
+    return `${parsedUrl.protocol}//${newDomain}${port}${pathname}${parsedUrl.search}${parsedUrl.hash}`;
+  } catch (err) {
+    console.error('Invalid URL:', err);
+    return url;
+  }
+};
